90193c8c1de6d4203d6a11065a59223d3e369c39
yuri <wtruth@sohu.com>
2016-02-17 23:41:19 +0800

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+#print 'hello world'
+#print (100 + 200)
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import platform;
+
+pythonVersion = platform.python_version();
+uname = platform.uname();
+
+print "Just for demo how to do python development under windows:";
+print "Current python version info is %s"%(pythonVersion);
+print "uname=",uname;
