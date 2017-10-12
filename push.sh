065d96084186e402b5add3cb7d044a3127c6b4cd
203monitor <930502900@qq.com>
2017-10-12 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -5,7 +5,13 @@
  git config --global user.email "930502900@qq.com"
  git config --global user.name "203monitor"
 
-  git commit -am "post" && git commit --amend --date='Sun, 14 Dec 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sun, 21 Dec 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Mon, 22 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Tue, 23 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Wed, 24 Jan 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Thu, 12 Oct 2017 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Fri, 26 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 28 Oct 2017 20:10:07 +0800' 
 
 # git commit -am "post" && git commit --amend --date='Sun, 21 Dec 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Mon, 22 Jan 2018 20:10:07 +0800' 
