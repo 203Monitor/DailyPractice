e51bd56191c956eb5a2435e71291c7f833102361
203Monitor <930502900@qq.com>
2017-11-03 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -2,12 +2,12 @@
 
 # Mon. Tue. Wed. Thu. Fri. Sat. Sun.
 
-# git commit -am "post" && git commit --amend --date='Sun, 26 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Mon, 27 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Tue, 28 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Wed, 29 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Thu, 30 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Fri, 1 Dec 2017 20:10:07 +0800' && ./push.sh
- git commit -am "post" && git commit --amend --date='Sat, 2 Dec 2017 20:10:07 +0800' && ./push.sh
+ git commit -am "post" && git commit --amend --date='Sun, 3 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Mon, 4 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Tue, 5 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Wed, 6 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Thu, 7 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Fri, 8 Dec 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Sat, 9 Dec 2017 20:10:07 +0800' && ./push.sh
 
  git push --force --tags origin 'refs/heads/*'
