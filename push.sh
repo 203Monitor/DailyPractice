46026a3bb621630b1ff2b308ddf97f68782f4784
203Monitor <930502900@qq.com>
2017-11-29 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -4,8 +4,8 @@
 
 # git commit -am "post" && git commit --amend --date='Sun, 26 Nov 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Mon, 27 Nov 2017 20:10:07 +0800' && ./push.sh
- git commit -am "post" && git commit --amend --date='Tue, 28 Nov 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Wed, 29 Nov 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Tue, 28 Nov 2017 20:10:07 +0800' && ./push.sh
+ git commit -am "post" && git commit --amend --date='Wed, 29 Nov 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Thu, 30 Nov 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Fri, 1 Dec 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Sat, 2 Dec 2017 20:10:07 +0800' && ./push.sh
