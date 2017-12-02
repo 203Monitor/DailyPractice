4a1ad38d7bd76b55d7bbb3262328151057b57a8c
203Monitor <930502900@qq.com>
2017-12-02 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@
 # git commit -am "post" && git commit --amend --date='Tue, 28 Nov 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Wed, 29 Nov 2017 20:10:07 +0800' && ./push.sh
 # git commit -am "post" && git commit --amend --date='Thu, 30 Nov 2017 20:10:07 +0800' && ./push.sh
- git commit -am "post" && git commit --amend --date='Fri, 1 Dec 2017 20:10:07 +0800' && ./push.sh
-# git commit -am "post" && git commit --amend --date='Sat, 2 Dec 2017 20:10:07 +0800' && ./push.sh
+# git commit -am "post" && git commit --amend --date='Fri, 1 Dec 2017 20:10:07 +0800' && ./push.sh
+ git commit -am "post" && git commit --amend --date='Sat, 2 Dec 2017 20:10:07 +0800' && ./push.sh
 
  git push --force --tags origin 'refs/heads/*'
