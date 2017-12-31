09579fad50c6142d69d99381919462ebf16efff3
203monitor <930502900@qq.com>
2017-12-31 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -12,9 +12,9 @@
 # git commit -am "post" && git commit --amend --date='Wed, 27 Dec 2017 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Thu, 28 Dec 2017 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Fri, 29 Dec 2017 20:10:07 +0800' 
- git commit -am "post" && git commit --amend --date='Sat, 30 Dec 2017 20:10:07 +0800' 
-# 
-# git commit -am "post" && git commit --amend --date='Sun, 31 Dec 2017 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 30 Dec 2017 20:10:07 +0800' 
+ 
+ git commit -am "post" && git commit --amend --date='Sun, 31 Dec 2017 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Mon, 1 Jan 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Tue, 2 Jan 2017 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Wed, 3 Jan 2017 20:10:07 +0800' 
