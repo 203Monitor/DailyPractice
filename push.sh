13b733c6add98c083aec28d7d6cdb113a70f4715
203monitor <930502900@qq.com>
2018-02-25 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -6,12 +6,12 @@
  git config --global user.name "203monitor"
 
 
-# git commit -am "post" && git commit --amend --date='Sun, 18 Feb 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 19 Feb 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 20 Feb 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 21 Feb 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 22 Feb 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 23 Feb 2018 20:10:07 +0800' 
- git commit -am "post" && git commit --amend --date='Sat, 24 Feb 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Sun, 25 Feb 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Mon, 26 Feb 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Tue, 27 Feb 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Wed, 28 Feb 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Thu, 1 Mar 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Fri, 2 Mar 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 3 Mar 2018 20:10:07 +0800' 
  
  git push --force --tags origin 'refs/heads/*'
