306b0adddd41a38cee6d972a5c7fd5d01aefe5e4
203monitor <930502900@qq.com>
2018-03-02 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -10,8 +10,8 @@
 # git commit -am "post" && git commit --amend --date='Mon, 26 Feb 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Tue, 27 Feb 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Wed, 28 Feb 2018 20:10:07 +0800' 
- git commit -am "post" && git commit --amend --date='Thu, 1 Mar 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 2 Mar 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Thu, 1 Mar 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Fri, 2 Mar 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Sat, 3 Mar 2018 20:10:07 +0800' 
  
  git push --force --tags origin 'refs/heads/*'
