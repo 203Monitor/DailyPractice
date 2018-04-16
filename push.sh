7d80defed5034acda4a2fee70ad7f1cd01242da8
203monitor <930502900@qq.com>
2018-04-16 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -6,8 +6,8 @@
  git config --global user.name "203monitor"
 
 
- git commit -am "post" && git commit --amend --date='Sun, 15 Apr 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 16 Apr 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sun, 15 Apr 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Mon, 16 Apr 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Tue, 17 Apr 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Wed, 18 Apr 2018 20:10:07 +0800' 
 # git commit -am "post" && git commit --amend --date='Thu, 19 Apr 2018 20:10:07 +0800' 
