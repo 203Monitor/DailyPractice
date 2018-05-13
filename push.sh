40e18a4238f161eea3afba81f25a3d291aae1548
203monitor <930502900@qq.com>
2018-05-13 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -6,12 +6,22 @@
  git config --global user.name "203monitor"
 
 
-# git commit -am "post" && git commit --amend --date='Sun, 6 May 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 7 May 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 8 May 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 9 May 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 10 May 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 11 May 2018 20:10:07 +0800' 
- git commit -am "post" && git commit --amend --date='Sat, 12 May 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Sun, 13 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Mon, 14 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Tue, 15 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Wed, 16 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Thu, 17 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Fri, 18 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 19 May 2018 20:10:07 +0800' 
+ 
+# git commit -am "post" && git commit --amend --date='Sun, 20 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Mon, 21 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Tue, 22 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Wed, 23 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Thu, 24 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Fri, 25 May 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 26 May 2018 20:10:07 +0800' 
+
+# git commit -am "post" && git commit --amend --date='Sun, 27 May 2018 20:10:07 +0800' 
  
  git push --force --tags origin 'refs/heads/*'
