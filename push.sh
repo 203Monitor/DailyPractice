54255d67d40d1c033eb4ecb03fafdb900e8a00b3
203monitor <930502900@qq.com>
2018-12-21 20:10:07 +0800

    post

--- push.sh
+++ push.sh
@@ -5,37 +5,12 @@
  git config --global user.email "930502900@qq.com"
  git config --global user.name "203monitor"
 
-
-# git commit -am "post" && git commit --amend --date='Sun, 24 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 25 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 26 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 27 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 28 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 29 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Sat, 30 Dec 2017 20:10:07 +0800' 
- 
-# git commit -am "post" && git commit --amend --date='Sun, 31 Dec 2017 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 1 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 2 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 3 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 4 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 5 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Sat, 6 Jan 2018 20:10:07 +0800' 
-
-# git commit -am "post" && git commit --amend --date='Sun, 7 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 8 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 9 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 10 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 11 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 12 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Sat, 13 Jan 2018 20:10:07 +0800' 
-
-# git commit -am "post" && git commit --amend --date='Sun, 14 Dec 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Mon, 15 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Tue, 16 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Wed, 17 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Thu, 18 Jan 2018 20:10:07 +0800' 
-# git commit -am "post" && git commit --amend --date='Fri, 19 Jan 2018 20:10:07 +0800' 
- git commit -am "post" && git commit --amend --date='Sat, 20 Jan 2018 20:10:07 +0800' 
+ git commit -am "post" && git commit --amend --date='Sun, 21 Dec 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Mon, 22 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Tue, 23 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Wed, 24 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Thu, 25 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Fri, 26 Jan 2018 20:10:07 +0800' 
+# git commit -am "post" && git commit --amend --date='Sat, 27 Jan 2018 20:10:07 +0800' 
  
  git push --force --tags origin 'refs/heads/*'
